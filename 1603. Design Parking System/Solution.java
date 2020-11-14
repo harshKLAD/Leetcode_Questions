d7a2e527e3e3b6d15f08ae8ded269502721a9352
harshLAD <harshk.lad@gmail.com>
2020-11-14 13:46:41 -0500

    added

---
+++ 1603. Design Parking System/Solution.java
@@ -0,0 +1,50 @@
+class ParkingSystem {
+    int big;
+    int medium;
+    int small;
+    
+    HashMap<String, Integer> Spots = new HashMap<String, Integer>();
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+    
+    public boolean addCar(int carType) {
+        switch(carType) {
+            case 1:
+                if(big > 0){
+                    big--;
+                    return true;
+                }else{
+                    //return false;
+                    break;
+                }
+            case 2:
+                if(medium > 0){
+                    medium--;
+                    return true;
+                }else{
+                    //return false;
+                    break;
+                }
+            case 3:
+                if(small > 0){
+                    small--;
+                    return true;
+                }else{
+                    //return false;
+                    break;
+                }
+            
+        }
+        return false;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
