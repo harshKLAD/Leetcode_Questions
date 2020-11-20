b7748efc4fc065a8d7c63f51fe6e421bcfbd7570
harshLAD <harshk.lad@gmail.com>
2020-11-20 15:06:09 -0500

    Added new Q

---
+++ 1221. Split a String in Balanced Strings/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int L = 0;
+        int R = 0;
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'L') {
+                L++;
+            } else {
+                R++;
+            }
+
+            if (L == R) {
+                count++;
+                L = 0;
+                R = 0;
+            }
+        }
+        return count;
+    }
+}
